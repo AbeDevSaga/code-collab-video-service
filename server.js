f6cbde219d0f59cab1d4dcae7e42d46002dc8b7a
AbeDevSaga <2000amandanm@gmail.com>
2025-03-03 23:38:32 +0300

    credential removed

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin: '*', 
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
 }));
 connectDB();
 seedServices();
