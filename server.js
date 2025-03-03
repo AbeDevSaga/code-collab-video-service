219fd7b0d091821e91e43139fd1a6bd5f2ceb748
AbeDevSaga <2000amandanm@gmail.com>
2025-03-03 23:00:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow frontend access
+    methods: 'GET,POST,PUT,DELETE', // Allowed methods
+    allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+    credentials: true
+}));
 connectDB();
 seedServices();
 
