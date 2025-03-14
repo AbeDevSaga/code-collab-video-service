b0676c8cc28960bc420bbf3c81ccee06ba1ea48a
AbeDevSaga <2000amandanm@gmail.com>
2025-03-14 03:15:13 +0300

    seeding file removed

--- configuration/db_config.js
+++ configuration/db_config.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-const assignProjects = require("../seeds/seedProjects");
-
 
 dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB')
-    // await assignProjects();
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
     process.exit(1);
